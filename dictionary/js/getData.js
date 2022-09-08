89e177ea8ecfd7ffbe52fd1e8abc2c4a16111469
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2022-09-08 10:14:43 +0300

    fix all

--- dictionary/js/getData.js
+++ dictionary/js/getData.js
@@ -155,15 +155,12 @@ export default class GetData {
     const loadImg = document.getElementById('imageUpload')
     const previewImg = document.getElementById('img-preview')
     const imgFileName = document.getElementById("img-file-name")
-
     const loadAudio = document.getElementById('audioUpload')
     const previewAudio = document.getElementById('audio-preview')
     const audioFileName = document.getElementById('audio-file-name')
-
     const loadAudioExample = document.getElementById('audioExampleUpload')
     const previewAudioExample = document.getElementById('audioExample-preview')
     const audioExampleFileName = document.getElementById('audioExample-file-name')
-
     const loadAudioMeaning = document.getElementById('audioMeaningUpload')
     const previewAudioMeaning = document.getElementById('audioMeaning-preview')
     const audioMeaningFileName = document.getElementById('audioMeaning-file-name')
@@ -172,7 +169,6 @@ export default class GetData {
     loadAudio.addEventListener('change', e => this.handleFileUpload(e, previewAudio, audioFileName))
     loadAudioExample.addEventListener('change', e => this.handleFileUpload(e, previewAudioExample, audioExampleFileName))
     loadAudioMeaning.addEventListener('change', e => this.handleFileUpload(e, previewAudioMeaning, audioMeaningFileName))
-
     //Нажатие кнопки Отправить (запись слова в базу)
     formAddWord.addEventListener('submit', (event) => {
       event.preventDefault()
@@ -302,24 +298,20 @@ export default class GetData {
       textExampleTranslate: formAddWord.querySelector('#textExampleTranslate-update'),
       textMeaning: formAddWord.querySelector('#textMeaning-update'),
       textMeaningTranslate: formAddWord.querySelector('#textMeaningTranslate-update'),
-
       loadImg: document.getElementById('imageUpload-update'),
       previewImg: document.getElementById('img-preview-update'),
       imgFileName: document.getElementById("img-file-name-update"),
-
       loadAudio: document.getElementById('audioUpload-update'),
       previewAudio: document.getElementById('audio-preview-update'),
       audioFileName: document.getElementById('audio-file-name-update'),
-
       loadAudioExample: document.getElementById('audioExampleUpload-update'),
       previewAudioExample: document.getElementById('audioExample-preview-update'),
       audioExampleFileName: document.getElementById('audioExample-file-name-update'),
-
       loadAudioMeaning: document.getElementById('audioMeaningUpload-update'),
       previewAudioMeaning: document.getElementById('audioMeaning-preview-update'),
       audioMeaningFileName: document.getElementById('audioMeaning-file-name-update')
-
     }
+
     formElements.group.value = word.group
     formElements.page.value = word.page
     formElements.transcription.value = word.transcription
@@ -328,15 +320,14 @@ export default class GetData {
     formElements.textExampleTranslate.value = word.textExampleTranslate
     formElements.textMeaning.value = word.textMeaning
     formElements.textMeaningTranslate.value = word.textMeaningTranslate
-    
     formElements.previewImg.src = `${pathToFiles}${word.image}`
-    formElements.imgFileName.textContent = word.image
-    formElements.previewAudio.src =`${pathToFiles}${word.audio}`
-    formElements.audioFileName.textContent = word.audio
-    formElements.previewAudioExample.src =`${pathToFiles}${word.audioExample}`
-    formElements.audioExampleFileName.textContent = word.audioExample
-    formElements.previewAudioMeaning.src =`${pathToFiles}${word.audioMeaning}`
-    formElements.audioMeaningFileName.textContent = word.audioMeaning
+    formElements.imgFileName.textContent = word.image.replace("files/","")
+    formElements.previewAudio.src = `${pathToFiles}${word.audio}`
+    formElements.audioFileName.textContent = word.audio.replace("files/","")
+    formElements.previewAudioExample.src = `${pathToFiles}${word.audioExample}`
+    formElements.audioExampleFileName.textContent = word.audioExample.replace("files/","")
+    formElements.previewAudioMeaning.src = `${pathToFiles}${word.audioMeaning}`
+    formElements.audioMeaningFileName.textContent = word.audioMeaning.replace("files/","")
 
     formElements.loadImg.addEventListener('change', e => this.handleFileUpload(e, formElements.previewImg, formElements.imgFileName)) //выбор изображения
     formElements.loadAudio.addEventListener('change', e => this.handleFileUpload(e, formElements.previewAudio, formElements.audioFileName))
@@ -361,23 +352,16 @@ export default class GetData {
       event.preventDefault()
       const findWord = { word: findWordInput.value.trim() }
       submitBtnFindWord.disabled = true
-
       if (!this.isWordExists(findWord)) {
         showModal(`Слова ${findWord.word} не существует!`, 'error')
         resetFormFind()
       } else {
         findWordInput.disabled = true
         let editWord = this.getWord(findWord)[0]
-
-        console.log(editWord)
-        
         const formWordUpdate = document.getElementById('form-word-update')
-
         formWordUpdate.classList.remove('hide')
-        
         let formElements = this.showWordInForm(formWordUpdate, editWord)
         //Нажатие кнопки Отправить (запись слова в базу)
-        
         formWordUpdate.addEventListener('submit', (event) => {
           event.preventDefault()
           const updateWord = {
@@ -386,17 +370,14 @@ export default class GetData {
             transcription: formElements.transcription.value.trim(),
             wordTranslate: formElements.wordTranslate.value.trim(),
             image: `files/${formElements.imgFileName.textContent}`,
-            // // audio: audioUrl,
+            audio: `files/${formElements.audioFileName.textContent}`,
             textExample: formElements.textExample.value.trim(),
             textExampleTranslate: formElements.textExampleTranslate.value.trim(),
-            // // audioExample: audioExampleUrl,
+            audioExample:`files/${formElements.audioExampleFileName.textContent}`,
             textMeaning: formElements.textMeaning.value.trim(),
             textMeaningTranslate: formElements.textMeaningTranslate.value.trim(),
-            // // audioMeaning: audioMeaningUrl
+            audioMeaning: `files/${formElements.audioMeaningFileName.textContent}`
           }
-
-          console.log(updateWord)
-          
           this.updateWordInDB(editWord.word, updateWord)
           showModal(`Слово ${editWord.word} обновлено в словаре!`, 'valid')
           formFindWord.scrollIntoView({ block: "start", behavior: "smooth" })
--- dictionary/js/routing.js
+++ dictionary/js/routing.js
@@ -3,51 +3,41 @@ export default class Routing {
     this.toDictionaryButton = document.getElementById('dictionary-show')
     this.toHomeButton = document.getElementById('to-home-page')
     this.toEditPage = document.getElementById('to-edit-page')
-   
     this.homePage = document.getElementById('home-page')
     this.dictionaryPage = document.getElementById('dictionary-page')
     this.editPage = document.getElementById('edit-page')
-   
     this.editPageButtons = document.getElementById('edit-page-buttons')
-   //элементы страницы Добавления
-this.toAddWordButton = document.getElementById('to-add-word')
-    
+    //элементы страницы Добавления
+    this.toAddWordButton = document.getElementById('to-add-word')
     this.addWordPage = document.getElementById('add-word')
     this.formAddWord = document.getElementById('form-add-word')
     this.fromAddPageButton = document.getElementById('from-add-page')
-    this.submitBtnAddWord = document.getElementById('submit-btn-add-word') 
-   //элементы страницу удаления
-this.toDeleteWordButton = document.getElementById('to-delete-word')
-
+    this.submitBtnAddWord = document.getElementById('submit-btn-add-word')
+    //элементы страницу удаления
+    this.toDeleteWordButton = document.getElementById('to-delete-word')
     this.deleteWordPage = document.getElementById('delete-word')
     this.formDeleteWord = document.getElementById('form-delete-word')
     this.fromDeletePageButton = document.getElementById('from-delete-page')
-//элементы страницы обновления
-this.toEditWordButton = document.getElementById('to-edit-word')
-this.updateWordPage = document.getElementById('update-word')
-this.fromUpdatePageButton = document.getElementById('from-update-page')
-this.formFindWord = document.getElementById('form-find-word')
-this.formWordUpdate = document.getElementById('form-word-update')
-this.submitBtnFindWord = document.getElementById('submit-btn-find-word')
-this.findWordInput = document.querySelector('#find-word')
-
+    //элементы страницы обновления
+    this.toEditWordButton = document.getElementById('to-edit-word')
+    this.updateWordPage = document.getElementById('update-word')
+    this.fromUpdatePageButton = document.getElementById('from-update-page')
+    this.formFindWord = document.getElementById('form-find-word')
+    this.formWordUpdate = document.getElementById('form-word-update')
+    this.submitBtnFindWord = document.getElementById('submit-btn-find-word')
+    this.findWordInput = document.querySelector('#find-word')
+    //выход из редактирования
     this.exit = document.getElementById('exit')
- 
 
     this.toDictionaryButton.addEventListener('click', () => this.showDictionary())
     this.toHomeButton.addEventListener('click', () => this.showHome())
     this.toEditPage.addEventListener('click', () => this.showEdit())
-   
     this.toAddWordButton.addEventListener('click', () => this.showAddWord())
     this.fromAddPageButton.addEventListener('click', () => this.fromAddPage())
-   
     this.toDeleteWordButton.addEventListener('click', () => this.showDeleteWord())
     this.fromDeletePageButton.addEventListener('click', () => this.fromDeletePage())
-
-    this.toEditWordButton.addEventListener('click', ()=> this.showUpdateWord())
-    this.fromUpdatePageButton.addEventListener('click',()=> this.fromUpdatePage())
-    
-
+    this.toEditWordButton.addEventListener('click', () => this.showUpdateWord())
+    this.fromUpdatePageButton.addEventListener('click', () => this.fromUpdatePage())
     this.exit.addEventListener('click', () => location.reload())
   }
 
@@ -110,26 +100,26 @@ this.findWordInput = document.querySelector('#find-word')
     this.deleteWordPage.classList.remove("hide")
   }
 
-    //выход из режима удаления слова из словаря
-  fromDeletePage(){
+  //выход из режима удаления слова из словаря
+  fromDeletePage() {
     this.formDeleteWord.reset()
     this.deleteWordPage.classList.add('hide')
     this.editPageButtons.classList.remove('hide')
-   }
+  }
 
-   //отображение страницы обновления слова
-   showUpdateWord(){
+  //отображение страницы обновления слова
+  showUpdateWord() {
     this.editPageButtons.classList.add("hide")
     this.updateWordPage.classList.remove("hide")
-   }
-   //выход из режима обновления слова
-   fromUpdatePage(){
+  }
+  //выход из режима обновления слова
+  fromUpdatePage() {
     this.formFindWord.reset()
     this.updateWordPage.classList.add("hide")
     this.editPageButtons.classList.remove('hide')
     this.formWordUpdate.reset()
-    this.submitBtnFindWord.disabled=false 
-    this.findWordInput.disabled=false
+    this.submitBtnFindWord.disabled = false
+    this.findWordInput.disabled = false
     this.formWordUpdate.classList.add("hide")
-   }
+  }
 } 
